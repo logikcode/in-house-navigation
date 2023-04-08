50c53bd5515dcb9143669e8b100ecdd2bb208ecc
Emmanuel Oladipupo <emmanuel.oladipupo@decagon.dev>
2023-04-08 10:03:04 +0100

    add exceptions handlers

---
+++ mobile-station-service/src/main/java/com/navigation/mobilestationservice/exceptionhandler/ExceptionController.java
@@ -0,0 +1,26 @@
+package com.navigation.mobilestationservice.exceptionhandler;
+
+import com.navigation.mobilestationservice.exceptionhandler.exceptions.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Object> handleCityNotFoundException(EntityNotFoundException ex, WebRequest request) {
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+}

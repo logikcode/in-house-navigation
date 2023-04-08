aceafc65a2be9f9978c37b9fa875a6e87ae5f8f6
Emmanuel Oladipupo <emmanuel.oladipupo@decagon.dev>
2023-04-08 11:36:39 +0100

    created controller and implement endpoint for service functionalities

---
+++ mobile-station-service/src/main/java/com/navigation/mobilestationservice/controller/MobileStationController.java
@@ -0,0 +1,32 @@
+package com.navigation.mobilestationservice.controller;
+
+import com.navigation.mobilestationservice.model.MobileStation;
+import com.navigation.mobilestationservice.model.request.MobileStationRequestDto;
+import com.navigation.mobilestationservice.model.response.MobileStationResponseDto;
+import com.navigation.mobilestationservice.service.MobileStationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RequestMapping("/api/v1/mobile_station")
+@RestController
+@RequiredArgsConstructor
+public class MobileStationController {
+
+    private final MobileStationService mobileStationService;
+
+    @PostMapping()
+    public ResponseEntity<MobileStation> createMobileStation(@RequestBody MobileStationRequestDto mobileStationRequestDto){
+        return ResponseEntity.ok(mobileStationService.createBaseStation(mobileStationRequestDto));
+    }
+
+    @GetMapping("/location/{uuid}")
+    public ResponseEntity<MobileStationResponseDto> getMobileStation(@PathVariable("uuid") String id){
+        return ResponseEntity.ok(mobileStationService.getMobileStation(id));
+    }
+    @GetMapping("/all")
+    public List<MobileStation> retrieveStations(){
+        return mobileStationService.retrieveAllStationsInDB();
+    }
+}

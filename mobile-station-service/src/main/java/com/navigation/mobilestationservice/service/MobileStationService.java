56b80d3deae6252f5c4fd50f976504deea562803
Emmanuel Oladipupo <emmanuel.oladipupo@decagon.dev>
2023-04-08 11:37:44 +0100

    add interface for retrieving all mobileStations

--- mobile-station-service/src/main/java/com/navigation/mobilestationservice/service/MobileStationService.java
+++ mobile-station-service/src/main/java/com/navigation/mobilestationservice/service/MobileStationService.java
@@ -5,8 +5,12 @@ import com.navigation.mobilestationservice.model.MobileStation;
 import com.navigation.mobilestationservice.model.request.MobileStationRequestDto;
 import com.navigation.mobilestationservice.model.response.MobileStationResponseDto;
 
+import java.util.List;
+
 public interface MobileStationService {
     MobileStation createBaseStation(MobileStationRequestDto mobileStationRepository);
 
     MobileStationResponseDto getMobileStation(String id);
+
+    List<MobileStation> retrieveAllStationsInDB();
 }

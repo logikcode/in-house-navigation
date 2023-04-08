cec1688b7ad7e67009d8c05da62f25ad0d06527d
Emmanuel Oladipupo <emmanuel.oladipupo@decagon.dev>
2023-04-08 11:34:44 +0100

    add functionality for retrieving stations from other service using webclient

--- base-station-service/src/main/java/com/navigation/basestationservice/service/BaseStationServiceImpl.java
+++ base-station-service/src/main/java/com/navigation/basestationservice/service/BaseStationServiceImpl.java
@@ -2,20 +2,24 @@ package com.navigation.basestationservice.service;
 
 import com.navigation.basestationservice.exceptionhandler.exceptions.EntityNotFoundException;
 import com.navigation.basestationservice.model.BaseStation;
+import com.navigation.basestationservice.model.MobileStation;
 import com.navigation.basestationservice.model.request.BaseStationRequestDto;
 import com.navigation.basestationservice.model.response.BaseStationResponseDto;
 import com.navigation.basestationservice.repository.BaseStationRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.*;
 
 @Service
+@RequiredArgsConstructor
 public class BaseStationServiceImpl implements BaseStationService{
-    @Autowired
-    private BaseStationRepository baseStationRepository;
-    @Autowired
+
+    private final BaseStationRepository baseStationRepository;
+    private final WebClient.Builder webClietBuilder;
+
     @Override
     public BaseStation createBaseStation(BaseStationRequestDto baseStationRequestDto) {
         BaseStation baseStation = new BaseStation();
@@ -46,23 +50,46 @@ public class BaseStationServiceImpl implements BaseStationService{
        return responseDto;
     }
 
+
+
     private List<Map<String,Object>> getMobileStations(float x, float y, float detectionRadiusInMeters) {
-        List<MobileStation> mobileStations = mobileStationRepository.findAll();
-        List<Map<String,Object>> response = new ArrayList<>();
 
-                for(MobileStation mobileStation: mobileStations) {
-                    Map<String,Object> res = new HashMap<>();
-                    double distance = Math.sqrt(Math.pow(x - mobileStation.getLastKnownX(), 2)
-                            - Math.pow(y - mobileStation.getGetLastKnownY(), 2));
-                    if(distance <= detectionRadiusInMeters) {
-                        res.put("mobile_station_id", mobileStation.getId());
-                        res.put("distance", distance);
-                        res.put("timeStamp", mobileStation.getCreatedDate());
+        List<MobileStation> mobileStations = retrieveMobileStations();
+        return computeMobileStationDistance(mobileStations, x, y, detectionRadiusInMeters);
+    }
 
-                        response.add(res);
-                    }
+    public List<MobileStation> retrieveMobileStations(){
+        List<MobileStation> mobileStationList = new ArrayList<>();
+    MobileStation[] response;
+       response = webClietBuilder.build().get()
+                .uri("http://mobile-station-service/api/v1/mobile_station/all")
+                .retrieve()
+                .bodyToMono(MobileStation[].class)
+                .block();
 
-                }
-                return response;
+       assert response != null;
+       mobileStationList = Arrays.asList(response);
+       return mobileStationList;
+    }
+
+    private List<Map<String, Object>> computeMobileStationDistance(List<MobileStation> mobileStations, float x, float y, float detectionRadiusInMeters){
+
+        List<Map<String, Object>> response = new ArrayList<>();
+
+        for(MobileStation mobileStation: mobileStations) {
+            Map<String,Object> res = new HashMap<>();
+
+            double distance = Math.sqrt(Math.pow(x - mobileStation.getLastKnownX(), 2)
+                    - Math.pow(y - mobileStation.getGetLastKnownY(), 2));
+            if(distance <= detectionRadiusInMeters) {
+                res.put("mobile_station_id", mobileStation.getId());
+                res.put("distance", distance);
+                res.put("timeStamp", mobileStation.getCreatedDate());
+
+                response.add(res);
+            }
+
+        }
+        return response;
     }
 }

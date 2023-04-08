5414a4fdd00d56bbea7eb44d2a5c7011115660c8
Emmanuel Oladipupo <emmanuel.oladipupo@decagon.dev>
2023-04-08 09:49:28 +0100

    add BaseStationRepository

---
+++ base-station-service/src/main/java/com/navigation/basestationservice/repository/BaseStationRepository.java
@@ -0,0 +1,8 @@
+package com.navigation.basestationservice.repository;
+
+
+import com.navigation.basestationservice.model.BaseStation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BaseStationRepository extends JpaRepository<BaseStation, String> {
+}

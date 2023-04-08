73bc127b76e1b6b4c80bcd513c8d7232fe35c56d
Emmanuel Oladipupo <emmanuel.oladipupo@decagon.dev>
2023-04-08 11:31:45 +0100

    refactored the endpoint

--- base-station-service/src/main/java/com/navigation/basestationservice/controller/BaseStationController.java
+++ base-station-service/src/main/java/com/navigation/basestationservice/controller/BaseStationController.java
@@ -4,22 +4,26 @@ import com.navigation.basestationservice.model.BaseStation;
 import com.navigation.basestationservice.model.request.BaseStationRequestDto;
 import com.navigation.basestationservice.model.response.BaseStationResponseDto;
 import com.navigation.basestationservice.service.BaseStationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/v1/basestation")
+@RequestMapping("/api/v1/base_station")
+@RequiredArgsConstructor
 public class BaseStationController {
-    @Autowired
-private BaseStationService baseStationService;
-    @PostMapping()
+    private final BaseStationService baseStationService;
+    @PostMapping("/create")
     public ResponseEntity<BaseStation> createBaseStation(@RequestBody BaseStationRequestDto baseStationRequestDto){
         return ResponseEntity.ok(baseStationService.createBaseStation(baseStationRequestDto));
     }
 
-    @GetMapping()
+    @GetMapping("/get")
     public ResponseEntity<BaseStationResponseDto> getBaseStation(@RequestParam("id") String id){
         return ResponseEntity.ok(baseStationService.getBaseStation(id));
     }
+
 }
